064121d95f83514807b10154d221e61271b0b41d
RudraVB <gubsi2006@gmail.com>
2024-08-13 14:59:34 -0700

    Update Program3.java
    Found the area and the width by getting user input for length and width

--- Program3.java
+++ Program3.java
@@ -3,12 +3,44 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
-
+public class Program3{
+    public static void main(String[] args){
+        //declare variables
+        int length = 0;
+        int width = 0;
+        int area = 0;
+        int perimeter = 0;
+        
+        //create my scanner
+        Scanner numScan = new Scanner(System.in);
+        
+        //ask for user input
+        System.out.println("Enter The Lenght: ");
+        length = numScan.nextInt();
+        System.out.println("Enter The Width: ");
+        width = numScan.nextInt();
+        
+        //calculate area and perimeter
+        area = length*width;
+        perimeter = 2*(length+width);
+        
+        
+        
+        //print results
+        System.out.println("The area is: " + area);
+        System.out.println("The width is: " + perimeter);
+    }
+}
 
 
 
 //Paste console output below:
 /*
-
+Enter The Lenght: 
+143
+Enter The Width: 
+82
+The area is: 11726
+The width is: 450
 
 */
